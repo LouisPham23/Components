9bc2d792a81e6ebda3473f12a5643ba47cbf0942
Louis Pham <louispham23@gmail.com>
2020-02-17 16:55:03 -0800

    fixed navbar

--- src/components/Card/Card.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-
-const Card = () => {
-  return <h1>Hello world</h1>;
-};
-
-export default Card;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,13 +5,13 @@ import { IoIosMenu } from "react-icons/io";
 import { AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleClicked = () => {
     setIsOpen(!isOpen);
   };
   return (
-    <header className="bg-indigo-100">
+    <header className="bg-indigo-100 sm:flex sm:justify-between">
       <div className="bg-indigo-100 flex justify-between items-center px-4 py-3">
         <div className="flex items-center">
           <MdAccountBalance className="h-8 w-8 text-gray-800 block" />
@@ -36,34 +36,30 @@ const Navbar = () => {
       </div>
 
       <div
-        className={`${isOpen ? "block" : "hidden"} px-5 pt-2 pb-4 sm:block"`}
+        className={`${
+          isOpen ? "block" : "hidden"
+        } "px-5 pt-2 pb-4 sm:block sm:flex sm:pt-4`}
       >
         <Link
-          className="block text-gray-800 text-center hover:bg-gray-400 rounded"
+          className="block sm:ml-2 text-gray-800 text-center hover:bg-gray-400 rounded"
           to="/"
         >
           Home
         </Link>
         <Link
-          className="block text-gray-800 text-center hover:bg-gray-400 rounded"
-          to="/"
+          className="block sm:ml-2 text-gray-800 text-center hover:bg-gray-400 rounded"
+          to="/about"
         >
-          Balance
+          About Us
         </Link>
         <Link
-          className="block text-gray-800 text-center hover:bg-gray-400 rounded"
-          to="/"
-        >
-          Credit Cards
-        </Link>
-        <Link
-          className="block text-gray-800 text-center hover:bg-gray-400 rounded"
+          className="block sm:ml-2 text-gray-800 text-center hover:bg-gray-400 rounded"
           to="/login"
         >
           Login
         </Link>
         <Link
-          className="block text-gray-800 text-center hover:bg-gray-400 rounded"
+          className="block sm:mx-2 text-gray-800 text-center hover:bg-gray-400 rounded"
           to="/signup"
         >
           Sign Up
